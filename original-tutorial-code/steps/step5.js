779128508418d2d82e1dc6161b2891ba1ffd1a67
Colton Hurst <colton@coltonhurst.com>
2017-08-25 16:07:00 +0100

    Added step 5 and some refactoring.

--- original-tutorial-code/steps/step5.js
+++ original-tutorial-code/steps/step5.js
@@ -12,6 +12,22 @@ let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+let bricks = [];
+for (c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for (r = 0; r<brickRowCount; r++) {
+        bricks[c][r] = {x: 0, y: 0};
+    }
+}
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, pi * 2);
@@ -28,20 +44,44 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for (c=0; c<brickColumnCount; c++) {
+        for (r=0; r<brickRowCount; r++) {
+            let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            let brickY = (r*(brickHeight+brickPadding)) + brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
 function draw () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
     x+=dx;
     y+=dy;
-    
-    // bounds for bouncing (bottom/top)
-    if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-    }
+
     // bounds for bouncing (right/left)
     if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
+    }
+    // bounds for bouncing (bottom/top)
+    if (y + dy < ballRadius) {
+        dy = -dy;
+    } else if (y + dy > canvas.height-ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            alert("GAME OVER");
+            document.location.reload();
+        }
     }
 
     // check for key press
